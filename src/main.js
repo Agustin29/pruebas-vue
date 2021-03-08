c8d8429ccf2ad5f50cef5d20cb0150cad1672063
@agtbta29 <agustinbatista29@gmail.com>
2021-03-08 11:24:51 -0500

    cambios base

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+/* BOOTSTRAP */
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+  render: function(h) {
+    return h(App);
+  },
+}).$mount("#app");
